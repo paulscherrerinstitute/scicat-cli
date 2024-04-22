e928c26ac07016c0ff1ac3fcff53fefeae68e0db
Ali R. Vahdati <arv.kavir@gmail.com>
2024-04-22 16:45:20 +0200

    Refactor and test `AddAttachment`

--- datasetIngestor/addAttachment.go
+++ datasetIngestor/addAttachment.go
@@ -11,27 +11,28 @@ import (
 	"strings"
 )
 
-func AddAttachment(client *http.Client, APIServer string, datasetId string, metaDataDataset map[string]interface{}, accessToken string, attachmentFile string, caption string) {
-	// turn image into base64 structure:
+func ReadAndEncodeImage(attachmentFile string) (string, error) {
 	imgFile, err := os.Open(attachmentFile)
 	if err != nil {
-		log.Fatalf("Can not open attachment file %v \n", attachmentFile)
+		return "", err
 	}
-
 	defer imgFile.Close()
-
+	
 	// create a new buffer base on file size
 	fInfo, _ := imgFile.Stat()
 	var size int64 = fInfo.Size()
 	buf := make([]byte, size)
-
+	
 	// read file content into buffer
 	fReader := bufio.NewReader(imgFile)
 	fReader.Read(buf)
-
+	
 	// convert the buffer bytes to base64 string
 	imgBase64Str := base64.StdEncoding.EncodeToString(buf)
+	return imgBase64Str, nil
+}
 
+func CreateMetadataMap(datasetId string, caption string, metaDataDataset map[string]interface{}, imgBase64Str string) (map[string]interface{}, error) {
 	// assemble json structure
 	var metaDataMap map[string]interface{}
 	metaDataMap = make(map[string]interface{})
@@ -47,13 +48,26 @@ func AddAttachment(client *http.Client, APIServer string, datasetId string, meta
 			metaDataMap["accessGroups"], _ = accessGroups.([]interface{})
 		}
 	}
+	return metaDataMap, nil
+}
 
+func AddAttachment(client *http.Client, APIServer string, datasetId string, metaDataDataset map[string]interface{}, accessToken string, attachmentFile string, caption string) {
+	imgBase64Str, err := ReadAndEncodeImage(attachmentFile)
+	if err != nil {
+		log.Fatalf("Can not open attachment file %v \n", attachmentFile)
+	}
+	
+	metaDataMap, err := CreateMetadataMap(datasetId, caption, metaDataDataset, imgBase64Str)
+	if err != nil {
+		log.Fatal("Connect serialize meta data map:", metaDataMap)
+	}
+	
 	bm, err := json.Marshal(metaDataMap)
 	if err != nil {
 		log.Fatal("Connect serialize meta data map:", metaDataMap)
 	}
 	myurl := APIServer + "/Datasets/" + strings.Replace(datasetId, "/", "%2F", 1) + "/attachments?access_token=" + accessToken
-
+	
 	req, err := http.NewRequest("POST", myurl, bytes.NewBuffer(bm))
 	if err != nil {
 		log.Fatal(err)
