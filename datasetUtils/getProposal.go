a41d61dac8e8fc1689f2083f667b51a47c28e57b
Ali R. Vahdati <arv.kavir@gmail.com>
2024-05-13 12:54:38 +0200

    Add a unit test for `GetProposal`

--- datasetUtils/getProposal.go
+++ datasetUtils/getProposal.go
@@ -2,11 +2,28 @@ package datasetUtils
 
 import (
 	"encoding/json"
-	"io/ioutil"
+	"io"
 	"log"
 	"net/http"
 )
 
+/*
+GetProposal retrieves a proposal from a given API server. 
+
+Parameters:
+- client: An *http.Client object used to send the request.
+- APIServer: A string representing the base URL of the API server.
+- ownerGroup: A string representing the owner group of the proposal.
+- user: A map containing user information, including an access token.
+- accessGroups: A slice of strings representing the access groups of the user.
+
+The function constructs a filter based on the ownerGroup, then sends a GET request to the API server with the filter and user's access token. The response is then parsed into a map and returned.
+
+If the request or JSON unmarshalling fails, the function will log the error and terminate the program.
+
+Returns:
+- A map representing the proposal. If no proposal is found, an empty map is returned.
+*/
 func GetProposal(client *http.Client, APIServer string, ownerGroup string, user map[string]string,
 	accessGroups []string) (proposal map[string]interface{}) {
 	// Check if ownerGroup is in accessGroups list. No longer needed, done on server side and
@@ -40,7 +57,7 @@ func GetProposal(client *http.Client, APIServer string, ownerGroup string, user 
 		log.Fatal(err)
 	}
 	defer resp.Body.Close()
-	body, _ := ioutil.ReadAll(resp.Body)
+	body, _ := io.ReadAll(resp.Body)
 
 	// fmt.Printf("response Object:\n%v\n", string(body))
 
