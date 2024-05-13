97c679872a8eace897ee579e111fefaab0268c3f
Ali R. Vahdati <arv.kavir@gmail.com>
2024-05-13 09:50:45 +0200

    Add tests for `DetDatasetDetails`

--- datasetUtils/getDatasetDetails.go
+++ datasetUtils/getDatasetDetails.go
@@ -18,8 +18,21 @@ type Dataset struct {
 	NumberOfFiles int
 }
 
-// get dataset details and filter by ownergroup
+/*
+GetDatasetDetails retrieves details of datasets from a given API server. It filters the datasets by owner group if provided.
 
+Parameters:
+- client: The HTTP client used to send the request.
+- APIServer: The URL of the API server.
+- accessToken: The access token for authentication.
+- datasetList: A list of dataset IDs to retrieve details for.
+- ownerGroup: The owner group to filter the datasets by. If empty, no filtering is performed.
+
+The function sends HTTP GET requests to the API server in chunks of 100 datasets at a time. It constructs a filter query parameter for the request using the dataset IDs and the owner group. If the response status code is 200, it reads the response body and unmarshals the JSON into a slice of Dataset structs. It then checks if details were found for all datasets in the chunk. If details were found for a dataset and the owner group matches the filter (or no filter is provided), it adds the dataset to the output slice. If no details were found for a dataset, it logs a message. If the response status code is not 200, it logs an error message.
+
+Returns:
+- A slice of Dataset structs containing the details of the datasets that match the owner group filter.
+*/
 func GetDatasetDetails(client *http.Client, APIServer string, accessToken string, datasetList []string, ownerGroup string) []Dataset {
 	outputDatasetDetails := make([]Dataset, 0)
 	log.Println("Dataset ID                                         Size[MB]  Owner                      SourceFolder")
