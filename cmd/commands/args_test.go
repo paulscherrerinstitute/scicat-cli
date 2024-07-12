d5494a77ee7d8152f75540616ec2b6f77104f2db
consolethinks <consolethinks@gmail.com>
2024-07-12 10:51:21 +0200

    add args test to datasetIngestor

---
+++ cmd/commands/args_test.go
@@ -0,0 +1,67 @@
+package cmd
+
+import (
+	"testing"
+
+	"github.com/paulscherrerinstitute/scicat/datasetUtils"
+)
+
+func TestArgs(t *testing.T) {
+	// test cases
+	tests := []struct {
+		name         string
+		expectedArgs []interface{}
+		args         []string
+	}{
+		// datasetIngestor
+		{
+			name: "datasetIngestor test with 1 param",
+			expectedArgs: []interface{}{
+				"some/place/metadata.json",
+				"",
+				"",
+			},
+			args: []string{"datasetIngestor", "some/place/metadata.json"},
+		},
+		{
+			name: "datasetIngestor test with 2 params - folderlisting.txt",
+			expectedArgs: []interface{}{
+				"some/place/metadata.json",
+				"",
+				"folderlisting.txt",
+			},
+			args: []string{"datasetIngestor", "some/place/metadata.json", "folderlisting.txt"},
+		},
+		{
+			name: "datasetIngestor test with 2 params - filelisting",
+			expectedArgs: []interface{}{
+				"some/place/metadata.json",
+				"/some/path/",
+				"",
+			},
+			args: []string{"datasetIngestor", "some/place/metadata.json", "/some/path/"},
+		},
+	}
+
+	// running test cases
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			//flag.CommandLine = flag.NewFlagSet(test.name, flag.ExitOnError)
+			datasetUtils.TestArgs = func(args []interface{}) {
+				passing := true
+				for i, _ := range test.expectedArgs {
+					if test.expectedArgs[i] != args[i] {
+						t.Logf("'%v' is not correct, expected: '%s'", args[i], test.expectedArgs[i])
+						passing = false
+					}
+				}
+				if !passing {
+					t.Fail()
+				}
+			}
+
+			rootCmd.SetArgs(test.args)
+			Execute()
+		})
+	}
+}
