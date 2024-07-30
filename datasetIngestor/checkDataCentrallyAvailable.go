5af05b6298fb0da9b9b3c0022612b7362f7c5f91
Attila Martin Nacsa <attila.nacsa@etu.unige.ch>
2024-07-30 18:21:18 +0200

    avoid logs, return errors in checkDataCentrallyAvailable

--- datasetIngestor/checkDataCentrallyAvailable.go
+++ datasetIngestor/checkDataCentrallyAvailable.go
@@ -2,8 +2,6 @@ package datasetIngestor
 
 import (
 	"errors"
-	"log"
-	"os"
 	"os/exec"
 	"runtime"
 )
@@ -11,11 +9,14 @@ import (
 // execCommand is a variable that points to exec.Command, allowing it to be replaced in tests.
 var execCommand = exec.Command
 
-// CheckDataCentrallyAvailable checks if a specific directory (sourceFolder) is available on a remote server (ARCHIVEServer)
+// CheckDataCentrallyAvailableSsh checks if a specific directory (sourceFolder) is available on a remote server (ARCHIVEServer)
 // using the provided username for SSH connection. It returns an error if the directory is not available or if there's an issue with the SSH connection.
-func CheckDataCentrallyAvailable(username string, ARCHIVEServer string, sourceFolder string) (err error) {
+// Returned values:
+// - sshErr - the error returned by the ssh command
+// - err - other error that prevents the ssh command from being executed
+func CheckDataCentrallyAvailableSsh(username string, ARCHIVEServer string, sourceFolder string) (sshErr error, otherErr error) {
 	// NOTE why not use crypto/ssh ???
-	// NOTE is this even a reliable method for people outside PSI?
+	// NOTE even if the folder is there, not all files might be there!
 	var cmd *exec.Cmd
 
 	// Check the operating system
@@ -24,26 +25,26 @@ func CheckDataCentrallyAvailable(username string, ARCHIVEServer string, sourceFo
 		// Check if ssh exists
 		_, err := exec.LookPath("ssh") // locate a program in the user's path
 		if err != nil {
-			log.Println("SSH is not installed. Please install OpenSSH client.")
-			return err
+			return nil, errors.New("no ssh implementation is available")
 		}
 
 		// Create a new exec.Command to run the SSH command. The command checks if the directory exists on the remote server.
 		// The "-q" option suppresses all warnings, "-l" specifies the login name on the remote server.
 		cmd = execCommand("ssh", "-q", "-l", username, ARCHIVEServer, "test", "-d", sourceFolder)
 	default:
-		log.Printf("%s is not supported.\n", os)
-		return errors.New("unsupported operating system")
+		//log.Printf("%s is not supported.\n", os)
+		return nil, errors.New("unsupported operating system")
 	}
 
 	// Redirect the command's standard error to the process's standard error.
 	// This means that any error messages from the command will be displayed in the terminal.
-	cmd.Stderr = os.Stderr
+	// Update: We don't want a library to output to the terminal.
+	// cmd.Stderr = os.Stderr
 
 	// Log the command that is being run for debugging purposes.
-	log.Printf("Running %v.\n", cmd.Args)
+	//log.Printf("Running %v.\n", cmd.Args)
 
 	// Run the command and return any error that occurs.
-	err = cmd.Run()
-	return err
+	sshErr = cmd.Run()
+	return sshErr, nil
 }
