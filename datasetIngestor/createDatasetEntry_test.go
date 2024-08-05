465884631d32a66ce4402b6cbb42975494c2f524
Attila Martin Nacsa <attila.nacsa@etu.unige.ch>
2024-08-05 17:23:58 +0200

    refactor create/delete dataset entry

--- datasetIngestor/createDatasetEntry_test.go
+++ datasetIngestor/createDatasetEntry_test.go
@@ -8,7 +8,7 @@ import (
 
 func TestCreateDatasetEntry(t *testing.T) {
 	var capturedPath string
-	
+
 	// Create a mock server
 	server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
 		rw.Write([]byte(`{"pid": "1234"}`))
@@ -16,23 +16,26 @@ func TestCreateDatasetEntry(t *testing.T) {
 	}))
 	// Close the server when test finishes
 	defer server.Close()
-	
+
 	// Create a map for the metaData
 	metaDataMap := map[string]interface{}{
-		"type":                  "raw",
+		"type": "raw",
 	}
-	
+
 	// Create a client
 	client := server.Client()
-	
+
 	// Call the function
-	datasetId := CreateDatasetEntry(client, server.URL, metaDataMap, "testToken")
-	
+	datasetId, err := CreateDatasetEntry(client, server.URL, metaDataMap, "testToken")
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	}
+
 	// Check the returned datasetId
 	if datasetId != "1234" {
 		t.Errorf("Expected datasetId to be '1234', but got '%s'", datasetId)
 	}
-	
+
 	// Check the URL based on the type field
 	expectedPath := ""
 	switch metaDataMap["type"].(string) {
@@ -43,7 +46,7 @@ func TestCreateDatasetEntry(t *testing.T) {
 	case "base":
 		expectedPath = "/Datasets"
 	}
-	
+
 	// Check the URL
 	if capturedPath != expectedPath {
 		t.Errorf("Expected path to be '%s', but got '%s'", expectedPath, capturedPath)
--- datasetIngestor/deleteDatasetEntry.go
+++ datasetIngestor/deleteDatasetEntry.go
@@ -1,18 +1,22 @@
 package datasetIngestor
 
 import (
-	"log"
 	"net/http"
 	"strings"
 )
 
-func DeleteDatasetEntry(client *http.Client, APIServer string, datasetId string, accessToken string) {
+// note: this function is unused in cmd after a change in datasetIngestor command
+func DeleteDatasetEntry(client *http.Client, APIServer string, datasetId string, accessToken string) error {
 	req, err := http.NewRequest("DELETE", APIServer+"/Datasets/"+strings.Replace(datasetId, "/", "%2F", 1)+"?access_token="+accessToken, nil)
+	if err != nil {
+		return err
+	}
 	// fmt.Printf("Request:%v\n",req)
 	resp, err := client.Do(req)
 	// fmt.Printf("resp %v %v\n",resp.Body,resp.StatusCode)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 	defer resp.Body.Close()
+	return nil
 }
