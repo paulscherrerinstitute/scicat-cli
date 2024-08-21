5e047cde8ad5e7ce9790d7e23eac3872677c0f61
Philipp Wissmann <philipp.wissmann@ethz.ch>
2024-08-21 16:09:47 +0200

    Add wails.io based ingestor app
    Add project structure from wails init -t svelte-ts.
    Add OpenEM Logo and a sample button

---
+++ openem-ingestor/app.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"context"
+
+	"github.com/wailsapp/wails/v2/pkg/runtime"
+)
+
+// App struct
+type App struct {
+	ctx context.Context
+}
+
+// NewApp creates a new App application struct
+func NewApp() *App {
+	return &App{}
+}
+
+// Show prompt before closing the app
+func (b *App) beforeClose(ctx context.Context) (prevent bool) {
+	dialog, err := runtime.MessageDialog(ctx, runtime.MessageDialogOptions{
+		Type:    runtime.QuestionDialog,
+		Title:   "Quit?",
+		Message: "Are you sure you want to quit? This will stop all pending downloads.",
+	})
+
+	if err != nil {
+		return false
+	}
+	return dialog != "Yes"
+}
+
+// startup is called when the app starts. The context is saved
+// so we can call the runtime methods
+func (a *App) startup(ctx context.Context) {
+	a.ctx = ctx
+}
+
+func (a *App) SelectFolder() {
+
+}
