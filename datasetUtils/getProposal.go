d27323b663e2d13d7a7d8a7c469a3eac334594ac
consolethinks <consolethinks@gmail.com>
2024-10-04 17:55:30 +0200

    v4 updates for datasetGetProposal command

--- datasetUtils/getProposal.go
+++ datasetUtils/getProposal.go
@@ -2,13 +2,14 @@ package datasetUtils
 
 import (
 	"encoding/json"
-	"io"
 	"fmt"
+	"io"
 	"net/http"
+	"net/url"
 )
 
 /*
-GetProposal retrieves a proposal from a given API server. 
+GetProposal retrieves a proposal from a given API server.
 
 Parameters:
 - client: An *http.Client object used to send the request.
@@ -26,38 +27,40 @@ Returns:
 */
 func GetProposal(client *http.Client, APIServer string, ownerGroup string, user map[string]string,
 	accessGroups []string) (map[string]interface{}, error) {
-		
+
 	filter := fmt.Sprintf(`{"where":{"ownerGroup":"%s"}}`, ownerGroup)
-	url := fmt.Sprintf("%s/Proposals?access_token=%s", APIServer, user["accessToken"])
-	
+	url := fmt.Sprintf("%s/proposals?filters=%s", APIServer, url.QueryEscape(filter))
+
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		return nil, err
 	}
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("filter", filter)
-	
+	req.Header.Set("Authentication", "Bearer "+user["accessToken"])
+
 	resp, err := client.Do(req)
 	if err != nil {
 		return nil, err
 	}
 	defer resp.Body.Close()
-	
+	if resp.StatusCode < 200 || resp.StatusCode >= 300 {
+		return nil, fmt.Errorf("failing status code: %d", resp.StatusCode)
+	}
+
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		return nil, err
 	}
-	
+
 	var respObj []map[string]interface{}
 	err = json.Unmarshal(body, &respObj)
 	if err != nil {
 		return nil, err
 	}
-	
+
 	respMap := make(map[string]interface{})
 	if len(respObj) > 0 {
 		respMap = respObj[0]
 	}
-	
+
 	return respMap, nil
 }
