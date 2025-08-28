126b161f7cb59e2135a6ace5dbaf526dea73c4b6
Spencer Bliven <spencer.bliven@gmail.com>
2025-08-28 11:40:45 +0200

    Update dev URLs

--- cmd/commands/commonConstants.go
+++ cmd/commands/commonConstants.go
@@ -4,9 +4,9 @@ const MANUAL = "http://melanie.gitpages.psi.ch/SciCatPages"
 
 const PROD_API_SERVER string = "https://dacat.psi.ch/api/v3"
 const TEST_API_SERVER string = "https://dacat-qa.psi.ch/api/v3"
-const DEV_API_SERVER string = "https://dacat-development.psi.ch/api/v3"
+const DEV_API_SERVER string = "https://scicat.development.psi.ch/api/v3"
 const LOCAL_API_SERVER string = "http://backend.localhost/api/v3"
-const TUNNEL_API_SERVER string = "https://dacat-development.psi.ch:5443/api/v3"
+const TUNNEL_API_SERVER string = "https://scicat.development.psi.ch:5443/api/v3"
 
 const PROD_RSYNC_ARCHIVE_SERVER string = "pb-archive.psi.ch"
 const TEST_RSYNC_ARCHIVE_SERVER string = "pbt-archive.psi.ch"
@@ -14,5 +14,5 @@ const DEV_RSYNC_ARCHIVE_SERVER string = "arematest2in.psi.ch"
 const LOCAL_RSYNC_ARCHIVE_SERVER string = "localhost"
 const TUNNEL_RSYNC_ARCHIVE_SERVER string = "arematest2in.psi.ch:2022"
 
-const PUBLISHServer string = "doi2.psi.ch"
+const PUBLISHServer string = "doi.psi.ch"
 const RETRIEVELocation string = "/data/archiveManager/retrieve/"
