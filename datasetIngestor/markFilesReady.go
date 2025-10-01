3776623464f68a9b5c0028b25ba08da7c14e2ac3
Omkar Zade <omkar.zade@psi.ch>
2025-10-01 16:19:39 +0200

    (fix) use correct endpoint and payload for PATCH datasetlifecycle in MarkFilesReady (#140)

--- datasetIngestor/markFilesReady.go
+++ datasetIngestor/markFilesReady.go
@@ -9,7 +9,7 @@ import (
 )
 
 /*
-MarkFilesReady is a function that sends a PUT request to a specified API server to update the dataset lifecycle status.
+MarkFilesReady is a function that sends a PATCH request to a specified API server to update the dataset lifecycle status.
 
 Parameters:
 - client: An *http.Client object, used to send the HTTP request.
@@ -18,7 +18,7 @@ Parameters:
 - user: A map[string]string containing user information, specifically the access token.
 
 The function constructs a metadata map with the dataset lifecycle status set to "datasetCreated" and archivable set to true.
-This metadata is then converted to JSON and sent in the body of the PUT request.
+This metadata is then converted to JSON and sent in the body of the PATCH request.
 The URL for the request is constructed using the APIServer and datasetId parameters, and the user's access token is appended as a query parameter.
 
 If the request is successful (HTTP status code 200), the function logs a success message along with the response body.
@@ -26,14 +26,13 @@ If the request fails, the function logs a failure message along with the status 
 */
 func MarkFilesReady(client *http.Client, APIServer string, datasetId string, user map[string]string) error {
 	var metaDataMap = map[string]interface{}{}
-	metaDataMap["datasetlifecycle"] = map[string]interface{}{}
-	metaDataMap["datasetlifecycle"].(map[string]interface{})["archiveStatusMessage"] = "datasetCreated"
-	metaDataMap["datasetlifecycle"].(map[string]interface{})["archivable"] = true
+	metaDataMap["archiveStatusMessage"] = "datasetCreated"
+	metaDataMap["archivable"] = true
 
 	cmm, _ := json.Marshal(metaDataMap)
 	// metadataString := string(cmm)
 
-	myurl := APIServer + "/Datasets/" + url.QueryEscape(datasetId)
+	myurl := APIServer + "/Datasets/" + url.QueryEscape(datasetId) + "/datasetlifecycle"
 	req, err := http.NewRequest("PATCH", myurl, bytes.NewBuffer(cmm))
 	if err != nil {
 		return err
