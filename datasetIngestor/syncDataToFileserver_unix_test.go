b79164d23e9e91266692d169e3e4ca3b6249dd9e
Spencer Bliven <spencer.bliven@gmail.com>
2025-10-02 23:27:27 +0200

    Detect supported rsync flags from the help text
    Detection using only the version number wasn't reliable.
    
    ```
    # MacOS 14.5 builtin
    $  /usr/bin/rsync --help| egrep 'version [0-9]|--stderr|--msgs2stderr'
    rsync  version 2.6.9  protocol version 29
    
    # MacOS homebrew
    $ /opt/homebrew/bin/rsync --help| egrep 'version [0-9]|--stderr|--msgs2stderr'
    rsync  version 3.4.1  protocol version 32
    --stderr=e|a|c           change stderr output mode (default: errors)
    
    # RHEL 8.10
    $ rsync --help| egrep 'version [0-9]|--stderr|--msgs2stderr'
    rsync  version 3.1.3  protocol version 31
         --msgs2stderr           special output handling for debugging
    
    # RHEL 7.9
    $ rsync --help| egrep 'version [0-9]|--stderr|--msgs2stderr'
    rsync  version 3.1.2  protocol version 31
         --msgs2stderr           special output handling for debugging
    ```

--- datasetIngestor/syncDataToFileserver_unix_test.go
+++ datasetIngestor/syncDataToFileserver_unix_test.go
@@ -1,15 +1,16 @@
+//go:build aix || darwin || dragonfly || freebsd || (js && wasm) || linux || nacl || netbsd || openbsd || solaris
 // +build aix darwin dragonfly freebsd js,wasm linux nacl netbsd openbsd solaris
 
 package datasetIngestor
 
 import (
-	"testing"
 	"regexp"
 	"strings"
+	"testing"
 )
 
 func TestGetRsyncVersion(t *testing.T) {
-	version, err := getRsyncVersion()
+	version, err := getRsyncVersion("/usr/bin/rsync")
 	if err != nil {
 		t.Errorf("getRsyncVersion() returned an error: %v", err)
 	}
@@ -27,6 +28,7 @@ func TestBuildRsyncCmd(t *testing.T) {
 	tests := []struct {
 		name             string
 		versionNumber    string
+		stderrFlags      []string
 		absFileListing   string
 		fullSourceFolder string
 		serverConnectStr string
@@ -35,6 +37,7 @@ func TestBuildRsyncCmd(t *testing.T) {
 		{
 			name:             "rsync version >= 3.2.3, absFileListing not empty",
 			versionNumber:    "3.2.3",
+			stderrFlags:      []string{"--stderr=error"},
 			absFileListing:   "/path/to/file",
 			fullSourceFolder: "/source/folder",
 			serverConnectStr: "user@server:/dest/folder",
@@ -43,6 +46,7 @@ func TestBuildRsyncCmd(t *testing.T) {
 		{
 			name:             "rsync version < 3.2.3, absFileListing not empty",
 			versionNumber:    "3.2.2",
+			stderrFlags:      []string{"-q", "--msgs2stderr"},
 			absFileListing:   "/path/to/file",
 			fullSourceFolder: "/source/folder",
 			serverConnectStr: "user@server:/dest/folder",
@@ -51,6 +55,7 @@ func TestBuildRsyncCmd(t *testing.T) {
 		{
 			name:             "rsync version >= 3.2.3, absFileListing empty",
 			versionNumber:    "3.2.3",
+			stderrFlags:      []string{"--stderr=error"},
 			absFileListing:   "",
 			fullSourceFolder: "/source/folder",
 			serverConnectStr: "user@server:/dest/folder",
@@ -59,16 +64,27 @@ func TestBuildRsyncCmd(t *testing.T) {
 		{
 			name:             "rsync version < 3.2.3, absFileListing empty",
 			versionNumber:    "3.2.2",
+			stderrFlags:      []string{"-q", "--msgs2stderr"},
 			absFileListing:   "",
 			fullSourceFolder: "/source/folder",
 			serverConnectStr: "user@server:/dest/folder",
 			expectedCmd:      "/usr/bin/rsync -e ssh -avx --progress -q --msgs2stderr /source/folder user@server:/dest/folder",
 		},
+		{
+			name:             "rsync version < 3.2.3, absFileListing empty",
+			versionNumber:    "2.6.9",
+			stderrFlags:      []string{},
+			absFileListing:   "",
+			fullSourceFolder: "/source/folder",
+			serverConnectStr: "user@server:/dest/folder",
+			expectedCmd:      "/usr/bin/rsync -e ssh -avx --progress /source/folder user@server:/dest/folder",
+		},
 	}
-		
+
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			cmd := buildRsyncCmd(tt.versionNumber, tt.absFileListing, tt.fullSourceFolder, tt.serverConnectStr)
+			rsyncCmd := RsyncCmd{"/usr/bin/rsync", tt.versionNumber, tt.stderrFlags}
+			cmd := buildRsyncCmd(&rsyncCmd, tt.absFileListing, tt.fullSourceFolder, tt.serverConnectStr)
 			cmdStr := strings.Join(cmd.Args, " ")
 			if cmdStr != tt.expectedCmd {
 				t.Errorf("Expected command: %s, got: %s", tt.expectedCmd, cmdStr)
