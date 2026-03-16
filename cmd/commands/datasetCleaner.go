23817a17bb0284d327f0b620e624ce306343ea11
minottic <carlo.minotti1@gmail.com>
2026-03-16 15:04:16 +0100

    Move patch job to dedicate file

--- cmd/commands/datasetCleaner.go
+++ cmd/commands/datasetCleaner.go
@@ -1,14 +1,10 @@
 package cmd
 
 import (
-	"bytes"
 	"crypto/tls"
-	"encoding/json"
 	"fmt"
-	"io"
 	"log"
 	"net/http"
-	"net/url"
 	"time"
 
 	"github.com/fatih/color"
@@ -116,7 +112,7 @@ For further help see "` + MANUAL + `"`,
 
 		jobId, err := datasetUtils.RemoveFromArchive(client, APIServer, pid, user, nonInteractiveFlag)
 		if err != nil {
-			patchError := patchJobStatus(client, APIServer, user, jobId, "finishedUnsuccessful")
+			patchError := datasetUtils.PatchJobStatus(client, APIServer, user, jobId, "finishedUnsuccessful")
 			if patchError != nil {
 				log.Fatalf("Failed to patch job status: %v", patchError)
 			}
@@ -126,7 +122,7 @@ For further help see "` + MANUAL + `"`,
 		if removeFromCatalogFlag {
 			err = datasetUtils.RemoveFromCatalog(client, APIServer, pid, jobId, user, nonInteractiveFlag, 10)
 			if err != nil {
-				patchError := patchJobStatus(client, APIServer, user, jobId, "finishedUnsuccessful")
+				patchError := datasetUtils.PatchJobStatus(client, APIServer, user, jobId, "finishedUnsuccessful")
 				if patchError != nil {
 					log.Fatalf("Failed to patch job status: %v", patchError)
 				}
@@ -147,36 +143,4 @@ func init() {
 	datasetCleanerCmd.Flags().Bool("devenv", false, "Use development environment instead of production environment (developers only)")
 
 	datasetCleanerCmd.MarkFlagsMutuallyExclusive("testenv", "devenv")
-}
-
-func patchJobStatus(client *http.Client, APIServer string, user map[string]string, jobID string, status string) error {
-	myurl := fmt.Sprintf("%s/Jobs/%s", APIServer, url.PathEscape(jobID))
-	payload := map[string]string{
-		"jobStatusMessage": status,
-	}
-
-	jsonData, err := json.Marshal(payload)
-	if err != nil {
-		return fmt.Errorf("failed to marshal patch payload: %w", err)
-	}
-	req, err := http.NewRequest("PATCH", myurl, bytes.NewBuffer(jsonData))
-	if err != nil {
-		return fmt.Errorf("failed to create job status request: %w", err)
-	}
-
-	req.Header.Set("Authorization", "Bearer "+user["accessToken"])
-	req.Header.Set("Content-Type", "application/json")
-
-	resp, err := client.Do(req)
-	if err != nil {
-		return fmt.Errorf("network error on job status: %w", err)
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode >= 400 {
-		body, _ := io.ReadAll(resp.Body)
-		return fmt.Errorf("job status request failed (%d): %s", resp.StatusCode, string(body))
-	}
-
-	return nil
 }
