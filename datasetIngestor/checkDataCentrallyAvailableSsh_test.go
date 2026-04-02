9f56ab4b99a682199e9406daef60d002bd7afaf7
minottic <carlo.minotti1@gmail.com>
2026-04-02 13:32:22 +0200

    fix: distinguish between ssh errors and not found

--- datasetIngestor/checkDataCentrallyAvailableSsh_test.go
+++ datasetIngestor/checkDataCentrallyAvailableSsh_test.go
@@ -12,7 +12,9 @@ func TestCheckDataCentrallyAvailable(t *testing.T) {
 		username      string
 		archiveServer string
 		sourceFolder  string
-		wantErr       bool
+		isNotFoundErr bool
+		wantSshErr    bool
+		wantOtherErr  bool
 		errMsg        string
 	}{
 		{
@@ -20,29 +22,45 @@ func TestCheckDataCentrallyAvailable(t *testing.T) {
 			username:      "testuser",
 			archiveServer: "testserver",
 			sourceFolder:  "/test/folder",
-			wantErr:       false,
+			isNotFoundErr: false,
+			wantSshErr:    false,
+			wantOtherErr:  false,
 		},
 		{
 			name:          "test data not available",
 			username:      "testuser",
 			archiveServer: "testserver",
 			sourceFolder:  "/nonexistent/folder",
-			wantErr:       true,
+			isNotFoundErr: true,
+			wantSshErr:    true,
+			wantOtherErr:  false,
 			errMsg:        "exit status 1",
+		},
+		{
+			name:          "test remote check other failure",
+			username:      "testuser",
+			archiveServer: "testserver",
+			sourceFolder:  "/some/folder",
+			isNotFoundErr: false,
+			wantSshErr:    false,
+			wantOtherErr:  true,
+			errMsg:        "ssh transport failure",
 		},
 		// Add more test cases here.
 	}
 
 	oldNewDumbClient := newDumbClient
 	oldCheckRemoteDirectory := checkRemoteDirectory
+	oldIsRemoteDirectoryNotFound := isRemoteDirectoryNotFound
 	defer func() {
 		newDumbClient = oldNewDumbClient
 		checkRemoteDirectory = oldCheckRemoteDirectory
+		isRemoteDirectoryNotFound = oldIsRemoteDirectoryNotFound
 	}()
 
 	for _, tt := range tests {
 		var returnError error
-		if tt.wantErr {
+		if tt.wantSshErr || tt.wantOtherErr {
 			returnError = errors.New(tt.errMsg)
 		}
 
@@ -52,17 +70,23 @@ func TestCheckDataCentrallyAvailable(t *testing.T) {
 		checkRemoteDirectory = func(c *Client, sourceFolder string, sshOutput io.Writer) error {
 			return returnError
 		}
+		isRemoteDirectoryNotFound = func(err error) bool {
+			return tt.isNotFoundErr
+		}
 
 		t.Run(tt.name, func(t *testing.T) {
 			sshErr, otherErr := CheckDataCentrallyAvailableSsh(tt.username, tt.archiveServer, tt.sourceFolder, nil)
-			if otherErr != nil {
-				t.Errorf("other error encountered: %v", otherErr)
+			if (sshErr != nil) != tt.wantSshErr {
+				t.Errorf("CheckDataCentrallyAvailable() sshErr = %v, wantSshErr %v", sshErr != nil, tt.wantSshErr)
 			}
-			if (sshErr != nil) != tt.wantErr {
-				t.Errorf("CheckDataCentrallyAvailable() error = %v, wantErr %v", sshErr != nil, tt.wantErr)
+			if (otherErr != nil) != tt.wantOtherErr {
+				t.Errorf("CheckDataCentrallyAvailable() otherErr = %v, wantOtherErr %v", otherErr != nil, tt.wantOtherErr)
 			}
-			if sshErr != nil && tt.wantErr && sshErr.Error() != tt.errMsg {
+			if sshErr != nil && tt.wantSshErr && sshErr.Error() != tt.errMsg {
 				t.Errorf("CheckDataCentrallyAvailable() errMsg = %v, wantErrMsg %v", sshErr.Error(), tt.errMsg)
+			}
+			if otherErr != nil && tt.wantOtherErr && otherErr.Error() != tt.errMsg {
+				t.Errorf("CheckDataCentrallyAvailable() otherErrMsg = %v, wantErrMsg %v", otherErr.Error(), tt.errMsg)
 			}
 		})
 	}
